c56c374752306dd96f1d9304e558681b62efaaf3
Pauline-Trimor <paulinetrimor@gmail.com>
2018-05-12 16:09:05 -0400

    created initial method and shuffle method for game object

--- js/script.js
+++ js/script.js
@@ -7,33 +7,31 @@ let card_flip = 0; /*used to keep track of cards flipped*/
 
 
 $(document).ready(function(){
-	var game = { /*creating object game, the functions are methods of this object*/
-	cards: [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8], /*creating array of card values*/
+	var game = { //creating object called game
+		cards: [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8], //card property with array of values
+		
+		init: function(){ //initial method that calls the shuffle method
+			game.shuffle();
+		},
+		
+		shuffle: function(){ //shuffle method that shuffles the deck
+			var j= 0;
+			var x=0;
+			var i=0;
+    		for (i = 0; i< game.cards.length; i++) {
+        		j = Math.round(Math.random()*i);
+       			x = game.cards[i];
+       			game.cards[i] = game.cards[j];
+       			game.cards[j] = x;
+    		};
+    		game.cards.assign();
+			console.log('Shuffled Card Array: '+game.cards);		
 
-	/*initial function will call the shuffle function the cards*/
-	init: function(){ 
-		game.shuffle()
-	},
-	/*function that randomly shuffles the cards and assigns them to the div*/
-	shuffle: function(){
-		var j, x, i;
-    	for (i = game.cards.length - 1; i > 0; i--) {
-        	j = Math.floor(Math.random() *(i + 1));
-       		x = cards[i];
-       		cards[i] = cards[j];
-       		cards[j] = x;
-    	}
-    	game.assignCards();
-		console.log('Shuffled Card Array: '+game.cards);		
-
-	},
-
+	}
 	/*for(var i=0; i<17; i++){
 
 		$('#card'+i).text('yeah'+i);
 	}*/
-	
-	}
 });
 
 
