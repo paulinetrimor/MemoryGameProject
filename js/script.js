51a3192f93da60cbec07c0bd9115ee45deacd23c
Pauline-Trimor <paulinetrimor@gmail.com>
2018-05-27 20:47:30 -0400

    Added image to cards

--- js/script.js
+++ js/script.js
@@ -2,19 +2,29 @@
 
 $(document).ready(function(){
 
-	cards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]; //card property with array of values
+	// Declare global variables
+	var count = 0;
+	var cards = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 
-	var count=0;//************************************************************************************
-	var clicks=[];
+	// magic number; -1 denotes the default, unset value for a click
+	var clickArray = [-1, -1];
+
+	// there are 8 pairs, when pairs reach 0, game is over!
+	var numPairs = 8;
+
 	var timer = {
  		seconds: 0,
   		minutes: 0,
   		clearTime: -1
-	};//**********************************************************************************************
-		
+	};
+
+	// Object variable to hold reference to the first clicked card
+	var FirstCard = null;
+
 	function init(){ //initial method that calls the shuffle method
 			shuffle();
 			assign();
+			//TO DO: initializze timer
 	}
 		
 	function shuffle(){ //shuffle method that shuffles the deck
@@ -30,14 +40,122 @@ $(document).ready(function(){
 			console.log('Shuffled Card Array: '+ cards);		
 	}
 
-	function assign(){ //assigns new values of array into divs
-		for (i=0; i<cards.length; i++) {
+	function assign() {
+		for (i = 0; i < cards.length; i++) {
 
-			$(".back"+(i+1)).append(cards[i]);
-		};	
+			var cardId = "#card" + (i + 1);
+			var cardElement = $(cardId);
+			
+			// enclose value inside a <span> element
+			cardElement.html("<span>" + cards[i] + "</span>");
+
+			// DEBUG
+			 console.log("value = " + cards);
+
+			// let this function create the handlers dynamically
+			createHandler(cardElement);
+		}	
 	}
 
-	function clickHandlers(a){ //determines if values in cards are equal
+	function createHandler(cardElement) {
+		cardElement.click(function(e) {
+			var isEqual = false;
+
+			// To prevent clicking same card twice, disable!
+			$(this).off();
+
+			// Ensure count doesn't get too high
+			count = count % 100;
+			count++; 
+
+			// store value of card
+			cardValue = $(cardElement).children('span').text();
+
+			//flip the card when clicked
+			//$(cardElement).css("visibility", "hidden");
+			
+
+
+			// DEBUG, print expected value of card!
+			console.log("CardValue: " + cardValue);
+
+			// determine to set first or second click
+			if ((count % 2) == 1) { // odd, first click
+				clickArray[0] = cardValue;
+				// stash reference to this card's handler for later reactivation
+				FirstCard = $(this);
+			}
+
+			else { // even, second click
+				clickArray[1] = cardValue;
+
+				// cards are set, lets compare!			
+				if (compareCards(cardElement, cardValue)) {
+					console.log("[+] Match!");
+					// Do not turn card click back on, they are both out of the game!
+					numPairs--; // decrement pairs
+					if (numPairs == 0) {
+						console.log("Game over, you won!");
+						// GAME OVER!
+						// gameOver() needed
+					}
+				} 
+
+				else {
+					console.log("[-] No match, try again!");
+					// Turn this card, and the card pointed to by FirstCard on
+					createHandler(FirstCard); // activate the card clicked before this
+					createHandler($(this)); // activate this card again
+				}
+			}
+		}); // end cardElement.click()
+	}
+
+	// return 0 (False) for not equal, return 1 (True) for equal
+	function compareCards(cardElement, cardValue) {
+		// compareCards should never be called with both elements unset (-1)
+		// lets error check
+		var retVal = false;
+		var firstClick = clickArray[0];
+		var secondClick = clickArray[1];
+
+		if (firstClick < 0 || secondClick < 0) {
+			// DEBUG
+			console.log("ERROR, compareCards called on empty values: " + firstClick + ", " + secondClick);
+			return 0;
+		}
+
+		else {
+			// This is called a ternary statement; its a short-hand way of doing if-else statements.
+			// The corresponding if-else code is below for comparison.
+
+			retVal = (firstClick == secondClick) ? true : false;
+			
+			// OR
+			// (firstClick == secondClick) ? retVal = true : retVal = false;
+
+			// if (firstClick == secondClick) {
+			//  	retVal = true;
+			// } 
+
+			// else {
+			//  	retVal = false;
+			// }
+		}
+
+		// At the end, we must reset clickArray
+		clickArray[0] = -1;
+		clickArray[1] = -1;
+
+		// TODO: Decrement turns count
+		return retVal;
+	}
+
+	//flip function when clicked
+	//	jQuery(function(){ jQuery(".card").flip({ trigger: 'click' }); });
+
+
+	/*function clickHandlers(a){ //determines if values in cards are equal
 		count++;
 		click=a.currentTarget.innerHTML;  //gets the value of the current element that was clicked
 		//updateMoveCounter(); //update the moves
@@ -66,7 +184,7 @@ $(document).ready(function(){
 	}
 
 	//starts the timer as soon as card is clicked******************************************************
-	function startTimer(){
+	async function startTimer(){
 		if (timer.seconds === 59) {
     		timer.minutes++;
     		timer.seconds = 0;
@@ -87,17 +205,17 @@ $(document).ready(function(){
 	}
 
 	//resets timer
-	function resetTimer() {
+	/*function resetTimer() {
   		clearInterval(timer.clearTime);
   		timer.seconds = 0;
   		timer.minutes = 0;
   		$(".timer").text("0:00");
 
   		timer.clearTime = setInterval(startTimer, 1000);
-	}
+	}*/
 
 	/*function showMode() {
- 		mode.css("display", "block");
+ 		modal.css("display", "block");
 	}
 
 	// Removes last start from remaining stars, updates mode HTML
@@ -122,19 +240,21 @@ $(document).ready(function(){
   		if (moveCounter === hard || moveCounter === medium) {
     	removeStar();
   		}
-	}**************************************************************************************************/
-
+	}*/
 
 	init();
-	startTimer();//timer only shows 1 second..???
+	// Move this to init() once it works
+	// Hint: startTimer() will likely need to be an async. function. We will get to that later!
+	//startTimer();//timer only shows 1 second..???
 
-	var className = document.getElementsByClassName('num');
+	//var className = document.getElementsByClassName('num');
 
 	//creates an event listerner for all cards
-	for (var i = 0; i < className.length; i++) {
+	/*for (var i = 0; i < className.length; i++) {
    		className[i].addEventListener('click', clickHandlers, false);
-		}
-	});
+	}*/
+	}); // end $(document).ready()
+
 
 
 
